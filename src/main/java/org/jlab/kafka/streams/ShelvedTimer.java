a17c94388ab1ebe9276b76fd72cedada1846b00e
ryans <ryans@jlab.org>
2020-12-07 08:25:14 -0500

    Init source

---
+++ src/main/java/org/jlab/kafka/streams/ShelvedTimer.java
@@ -0,0 +1,161 @@
+package org.jlab.kafka.streams;
+
+public class ShelvedTimer {
+    private static final Logger LOGGER = Logger.getLogger("org.jlab.kafka.streams.ShelvedTimer");
+
+    public static final String INPUT_TOPIC = "registered-alarms";
+    public static final String OUTPUT_TOPIC = "epics-channels";
+
+    public static final Serde<String> INPUT_KEY_SERDE = Serdes.String();
+    public static final SpecificAvroSerde<RegisteredAlarm> INPUT_VALUE_SERDE = new SpecificAvroSerde<>();
+    public static final Serde<String> OUTPUT_KEY_SERDE = INPUT_KEY_SERDE;
+    public static final Serde<String> OUTPUT_VALUE_SERDE = INPUT_KEY_SERDE;
+
+    static Properties getStreamsConfig() {
+
+        String bootstrapServers = System.getenv("BOOTSTRAP_SERVERS");
+
+        bootstrapServers = (bootstrapServers == null) ? "localhost:9092" : bootstrapServers;
+
+        String registry = System.getenv("SCHEMA_REGISTRY");
+
+        registry = (registry == null) ? "http://localhost:8081" : registry;
+
+        final Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "registrations2epics");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0); // Disable caching
+        props.put(SCHEMA_REGISTRY_URL_CONFIG, registry);
+
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        return props;
+    }
+
+    /**
+     * Create the Kafka Streams Domain Specific Language (DSL) Topology.
+     *
+     * @param props The streams configuration
+     * @return The Topology
+     */
+    static Topology createTopology(Properties props) {
+        final StreamsBuilder builder = new StreamsBuilder();
+        Map<String, String> config = new HashMap<>();
+        config.put(SCHEMA_REGISTRY_URL_CONFIG, props.getProperty(SCHEMA_REGISTRY_URL_CONFIG));
+        INPUT_VALUE_SERDE.configure(config, false);
+
+        final StoreBuilder<KeyValueStore<String, RegisteredAlarm>> storeBuilder = Stores.keyValueStoreBuilder(
+                Stores.persistentKeyValueStore("Registrations2EpicsStore"),
+                INPUT_KEY_SERDE,
+                INPUT_VALUE_SERDE
+        ).withCachingEnabled();
+
+        builder.addStateStore(storeBuilder);
+
+        final KStream<String, RegisteredAlarm> input = builder.stream(INPUT_TOPIC, Consumed.with(INPUT_KEY_SERDE, INPUT_VALUE_SERDE));
+
+        final KStream<String, String> output = input.transform(new MsgTransformerFactory(storeBuilder.name()), storeBuilder.name());
+
+        output.to(OUTPUT_TOPIC, Produced.with(OUTPUT_KEY_SERDE, OUTPUT_VALUE_SERDE));
+
+        return builder.build();
+    }
+
+    private static String toJsonKey(String channel) {
+        return "{\"topic\":\"active-alarms\",\"channel\":\"" + channel + "\"}";
+    }
+
+    private static String toJsonValue(String outkey, RegisteredAlarm registration) {
+        return registration == null ? null : "{\"mask\":\"a\",\"outkey\":\"" + outkey + "\"}";
+    }
+
+    /**
+     * Entrypoint of the application.
+     *
+     * @param args The command line arguments
+     */
+    public static void main(String[] args) {
+        final Properties props = getStreamsConfig();
+        final Topology top = createTopology(props);
+        final KafkaStreams streams = new KafkaStreams(top, props);
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        // attach shutdown handler to catch control-c
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.start();
+            latch.await();
+        } catch (final Throwable e) {
+            System.exit(1);
+        }
+        System.exit(0);
+    }
+
+    /**
+     * Factory to create Kafka Streams Transformer instances; references a stateStore to maintain previous
+     * RegisteredAlarms.
+     */
+    private static final class MsgTransformerFactory implements TransformerSupplier<String, RegisteredAlarm, KeyValue<String, String>> {
+
+        private final String storeName;
+
+        /**
+         * Create a new MsgTransformerFactory.
+         *
+         * @param storeName The state store name
+         */
+        public MsgTransformerFactory(String storeName) {
+            this.storeName = storeName;
+        }
+
+        /**
+         * Return a new {@link Transformer} instance.
+         *
+         * @return a new {@link Transformer} instance
+         */
+        @Override
+        public Transformer<String, RegisteredAlarm, KeyValue<String, String>> get() {
+            return new Transformer<String, RegisteredAlarm, KeyValue<String, String>>() {
+                private KeyValueStore<String, RegisteredAlarm> store;
+
+                @Override
+                @SuppressWarnings("unchecked") // https://cwiki.apache.org/confluence/display/KAFKA/KIP-478+-+Strongly+typed+Processor+API
+                public void init(ProcessorContext context) {
+                    store = (KeyValueStore<String, RegisteredAlarm>) context.getStateStore(storeName);
+                }
+
+                @Override
+                public KeyValue<String, String> transform(String key, RegisteredAlarm value) {
+                    KeyValue<String, String> result = null; // null returned to mean no record - when not of type DirectCAAlarm OR when an unmatched tombstone is encountered
+
+                    String channel;
+
+                    if(value == null) { // Tombstone - we need most recent non-null registration to transform
+                        RegisteredAlarm previous = store.get(key);
+                        if(previous != null) { // We only store DirectCAAlarm, so no need to check type
+                            channel = ((DirectCAAlarm)previous.getProducer()).getPv();
+                            result = KeyValue.pair(toJsonKey(channel), toJsonValue(key, value));
+                        }
+                    } else if(value.getProducer() instanceof DirectCAAlarm) {
+                        channel = ((DirectCAAlarm) value.getProducer()).getPv();
+                        result = KeyValue.pair(toJsonKey(channel), toJsonValue(key, value));
+                        store.put(key, value);  // Store most recent non-null registration for each CA alarm (key)
+                    }
+
+                    return result;
+                }
+
+                @Override
+                public void close() {
+                    // Nothing to do
+                }
+            };
+        }
+    }
+}
---
+++ src/test/java/org/jlab/kafka/streams/ShelvedTimerTest.java
@@ -0,0 +1,60 @@
+package org.jlab.kafka.streams;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ShelvedTimerTest {
+    private TopologyTestDriver testDriver;
+    private TestInputTopic<String, RegisteredAlarm> inputTopic;
+    private TestOutputTopic<String, String> outputTopic;
+    private RegisteredAlarm alarm1;
+
+    @Before
+    public void setup() {
+        final Properties streamsConfig = Registrations2Epics.getStreamsConfig();
+        streamsConfig.put(SCHEMA_REGISTRY_URL_CONFIG, "mock://testing");
+        final Topology top = Registrations2Epics.createTopology(streamsConfig);
+        testDriver = new TopologyTestDriver(top, streamsConfig);
+
+        // setup test topics
+        inputTopic = testDriver.createInputTopic(Registrations2Epics.INPUT_TOPIC, Registrations2Epics.INPUT_KEY_SERDE.serializer(), Registrations2Epics.INPUT_VALUE_SERDE.serializer());
+        outputTopic = testDriver.createOutputTopic(Registrations2Epics.OUTPUT_TOPIC, Registrations2Epics.OUTPUT_KEY_SERDE.deserializer(), Registrations2Epics.OUTPUT_VALUE_SERDE.deserializer());
+
+        DirectCAAlarm direct = new DirectCAAlarm();
+        direct.setPv("channel1");
+        alarm1 = new RegisteredAlarm();
+        alarm1.setProducer(direct);
+        alarm1.setCategory(AlarmCategory.Magnet);
+        alarm1.setLocation(AlarmLocation.INJ);
+        alarm1.setDocurl("/");
+        alarm1.setEdmpath("/");
+    }
+
+    @After
+    public void tearDown() {
+        testDriver.close();
+    }
+
+    @Test
+    public void matchedTombstoneMsg() {
+        inputTopic.pipeInput("alarm1", alarm1);
+        inputTopic.pipeInput("alarm1", null);
+        KeyValue<String, String> result = outputTopic.readKeyValuesToList().get(1);
+        Assert.assertNull(result.value);
+    }
+
+
+    @Test
+    public void unmatchedTombstoneMsg() {
+        inputTopic.pipeInput("alarm1", null);
+        Assert.assertTrue(outputTopic.isEmpty()); // Cannot transform a tombstone without a prior registration!
+    }
+
+    @Test
+    public void regularMsg() {
+        inputTopic.pipeInput("alarm1", alarm1);
+        KeyValue<String, String> result = outputTopic.readKeyValue();
+        Assert.assertEquals("{\"topic\":\"active-alarms\",\"channel\":\"channel1\"}", result.key);
+        Assert.assertEquals("{\"mask\":\"a\",\"outkey\":\"alarm1\"}", result.value);
+    }
+}
