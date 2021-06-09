07f58e3d65210e26c3f3cc46754313bbbad563fb
ryans <ryans@jlab.org>
2021-06-09 16:27:40 -0400

    It almost sorta kinda works with latching

--- src/main/java/org/jlab/jaws/LatchRule.java
+++ src/main/java/org/jlab/jaws/LatchRule.java
@@ -1,15 +1,13 @@
 package org.jlab.jaws;
 
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Predicate;
-import org.apache.kafka.streams.kstream.Produced;
-import org.jlab.jaws.entity.OverriddenAlarmKey;
-import org.jlab.jaws.entity.OverriddenAlarmType;
-import org.jlab.jaws.entity.OverriddenAlarmValue;
+import org.apache.kafka.streams.kstream.*;
+import org.jlab.jaws.entity.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,6 +16,23 @@ import java.util.Properties;
 import static io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG;
 
 public class LatchRule extends AutoOverrideRule {
+
+    public static final String OUTPUT_TOPIC = "overridden-alarms";
+
+    public static final String INPUT_TOPIC_REGISTERED = "registered-alarms";
+    public static final String INPUT_TOPIC_ACTIVE = "active-alarms";
+
+    public static final Serdes.StringSerde INPUT_KEY_REGISTERED_SERDE = new Serdes.StringSerde();
+    public static final Serdes.StringSerde INPUT_KEY_ACTIVE_SERDE = new Serdes.StringSerde();
+
+    public static final SpecificAvroSerde<RegisteredAlarm> INPUT_VALUE_REGISTERED_SERDE = new SpecificAvroSerde<>();
+    public static final SpecificAvroSerde<ActiveAlarm> INPUT_VALUE_ACTIVE_SERDE = new SpecificAvroSerde<>();
+
+    public static final SpecificAvroSerde<OverriddenAlarmKey> OUTPUT_KEY_SERDE = new SpecificAvroSerde<>();
+    public static final SpecificAvroSerde<OverriddenAlarmValue> OUTPUT_VALUE_SERDE = new SpecificAvroSerde<>();
+
+    public static final SpecificAvroSerde<RegisteredActive> REGISTERED_ACTIVE_VALUE_SERDE = new SpecificAvroSerde<>();
+
     @Override
     public Properties constructProperties() {
         final Properties props = super.constructProperties();
@@ -29,6 +44,55 @@ public class LatchRule extends AutoOverrideRule {
 
     @Override
     public Topology constructTopology(Properties props) {
-        return null;
+        final StreamsBuilder builder = new StreamsBuilder();
+
+        // If you get an unhelpful NullPointerException in the depths of the AVRO deserializer it's likely because you didn't set registry config
+        Map<String, String> config = new HashMap<>();
+        config.put(SCHEMA_REGISTRY_URL_CONFIG, props.getProperty(SCHEMA_REGISTRY_URL_CONFIG));
+
+        INPUT_VALUE_REGISTERED_SERDE.configure(config, false);
+        INPUT_VALUE_ACTIVE_SERDE.configure(config, false);
+
+        OUTPUT_KEY_SERDE.configure(config, true);
+        OUTPUT_VALUE_SERDE.configure(config, false);
+
+        REGISTERED_ACTIVE_VALUE_SERDE.configure(config, false);
+
+        final KTable<String, RegisteredAlarm> registeredTable = builder.table(INPUT_TOPIC_REGISTERED,
+                Consumed.as("Registered-Table").with(INPUT_KEY_REGISTERED_SERDE, INPUT_VALUE_REGISTERED_SERDE));
+        final KTable<String, ActiveAlarm> activeTable = builder.table(INPUT_TOPIC_ACTIVE,
+                Consumed.as("Active-Table").with(INPUT_KEY_ACTIVE_SERDE, INPUT_VALUE_ACTIVE_SERDE));
+
+
+        KTable<String, RegisteredActive> registeredActive = registeredTable.join(activeTable, new RegisteredActiveJoiner(), Materialized.with(Serdes.String(), REGISTERED_ACTIVE_VALUE_SERDE));
+
+        // Now map into overridden-alarms topic format conditionally
+        final KStream<OverriddenAlarmKey, OverriddenAlarmValue> out = registeredActive.toStream().map(new KeyValueMapper<String, RegisteredActive, KeyValue<? extends OverriddenAlarmKey, ? extends OverriddenAlarmValue>>() {
+            @Override
+            public KeyValue<? extends OverriddenAlarmKey, ? extends OverriddenAlarmValue> apply(String key, RegisteredActive value) {
+                KeyValue<OverriddenAlarmKey, OverriddenAlarmValue> record = null;
+
+                if(value.getActive() && value.getLatching()) {
+                    record = new KeyValue<>(new OverriddenAlarmKey(key, OverriddenAlarmType.Latched), new OverriddenAlarmValue(new LatchedAlarm()));
+                }
+
+                return record;
+            }
+        }, Named.as("Map-Latch"));
+
+        out.to(OUTPUT_TOPIC, Produced.as("Overridden-Alarms")
+                .with(OUTPUT_KEY_SERDE, OUTPUT_VALUE_SERDE));
+
+        return builder.build();
+    }
+
+    public static class RegisteredActiveJoiner implements ValueJoiner<RegisteredAlarm, ActiveAlarm, RegisteredActive> {
+
+        public RegisteredActive apply(RegisteredAlarm registered, ActiveAlarm active) {
+            return RegisteredActive.newBuilder()
+                    .setActive(active != null)
+                    .setLatching(registered == null ? false : registered.getLatching())
+                    .build();
+        }
     }
 }
---
+++ src/test/java/org/jlab/jaws/LatchRuleTest.java
@@ -0,0 +1,86 @@
+package org.jlab.jaws;
+
+import org.apache.kafka.streams.*;
+import org.jlab.jaws.entity.*;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import static io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG;
+
+public class LatchRuleTest {
+    private TopologyTestDriver testDriver;
+    private TestInputTopic<String, RegisteredAlarm> inputTopicRegistered;
+    private TestInputTopic<String, ActiveAlarm> inputTopicActive;
+    private TestOutputTopic<OverriddenAlarmKey, OverriddenAlarmValue> outputTopic;
+    private RegisteredAlarm registered1;
+    private RegisteredAlarm registered2;
+    private ActiveAlarm active1;
+    private ActiveAlarm active2;
+
+    @Before
+    public void setup() {
+        final LatchRule rule = new LatchRule();
+
+        final Properties props = rule.constructProperties();
+        props.put(SCHEMA_REGISTRY_URL_CONFIG, "mock://testing");
+        final Topology top = rule.constructTopology(props);
+        testDriver = new TopologyTestDriver(top, props);
+
+        // setup test topics
+        inputTopicRegistered = testDriver.createInputTopic(LatchRule.INPUT_TOPIC_REGISTERED, LatchRule.INPUT_KEY_REGISTERED_SERDE.serializer(), LatchRule.INPUT_VALUE_REGISTERED_SERDE.serializer());
+        inputTopicActive = testDriver.createInputTopic(LatchRule.INPUT_TOPIC_ACTIVE, LatchRule.INPUT_KEY_ACTIVE_SERDE.serializer(), LatchRule.INPUT_VALUE_ACTIVE_SERDE.serializer());
+        outputTopic = testDriver.createOutputTopic(ShelveExpirationRule.OUTPUT_TOPIC, ShelveExpirationRule.OUTPUT_KEY_SERDE.deserializer(), ShelveExpirationRule.OUTPUT_VALUE_SERDE.deserializer());
+
+        registered1 = new RegisteredAlarm();
+        registered2 = new RegisteredAlarm();
+
+        registered1.setClass$(AlarmClass.Base_Class);
+        registered1.setProducer(new SimpleProducer());
+        registered1.setLatching(true);
+
+        registered2.setClass$(AlarmClass.Base_Class);
+        registered2.setProducer(new SimpleProducer());
+        registered2.setLatching(false);
+
+        active1 = new ActiveAlarm();
+        active2 = new ActiveAlarm();
+
+        active1.setMsg(new SimpleAlarming());
+        active2.setMsg(new SimpleAlarming());
+    }
+
+    @After
+    public void tearDown() {
+        testDriver.close();
+    }
+
+    @Test
+    public void tombstoneMsg() throws InterruptedException {
+    }
+
+    @Test
+    public void notLatching() throws InterruptedException {
+    }
+
+    /*@Test
+    public void latching() {
+        inputTopicActive.pipeInput("alarm1", active1);
+        testDriver.advanceWallClockTime(Duration.ofSeconds(10));
+        inputTopicRegistered.pipeInput("alarm1", registered1);
+        List<KeyValue<OverriddenAlarmKey, OverriddenAlarmValue>> results = outputTopic.readKeyValuesToList();
+        Assert.assertEquals(1, results.size());
+
+        KeyValue<OverriddenAlarmKey, OverriddenAlarmValue> result = results.get(0);
+
+        Assert.assertEquals("alarm1", result.key.getName());
+        Assert.assertNull(result.value);
+    }*/
+}
