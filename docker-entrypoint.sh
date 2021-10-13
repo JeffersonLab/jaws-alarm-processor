168d7b1f3f1a12fdb15b498e9b7e755a8ca82ae6
slominskir <Ryan.Slominski@gmail.com>
2021-10-13 09:54:05 -0400

    now look for alarm-overrides-value

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,8 @@ echo "Step 1: Waiting for Schema Registry to start listening"
 echo "------------------------------------------------------"
 url=$SCHEMA_REGISTRY
 echo "waiting on: $url"
-while [ $(curl -s -o /dev/null -w %{http_code} $url/subjects/overridden-alarms-value/versions) -ne 200 ] ; do
-  echo -e $(date) " Kafka Registry listener HTTP state: " $(curl -s -o /dev/null -w %{http_code} $url/subjects/overridden-alarms-value/versions) " (waiting for 200)"
+while [ $(curl -s -o /dev/null -w %{http_code} $url/subjects/alarm-overrides-value/versions) -ne 200 ] ; do
+  echo -e $(date) " Kafka Registry listener HTTP state: " $(curl -s -o /dev/null -w %{http_code} $url/subjects/alarm-overrides-value/versions) " (waiting for 200)"
   sleep 5
 done
 
